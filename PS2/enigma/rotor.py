87b01f174200f68ad4927f0cf2e4d30f82949ac7
Matthew Grzegorczyk <matthewgrzegorczyk@gmail.com>
2018-04-12 23:29:42 +0200

    Get letter by index and by base letter.

--- PS2/enigma/rotor.py
+++ PS2/enigma/rotor.py
@@ -20,12 +20,32 @@ class Rotor:
 
         return self.alphabet[index]
 
+    def index(self, idx):
+        return self.shift + idx % len(self.alphabet)
+
+    def get_letter_by_index(self, idx):
+        index = self.index(idx)
+
+        return self.alphabet[index]
+
+    def get_letter_by_base_letter(self, letter):
+        letter = letter.upper()
+        idx = string.ascii_uppercase.index(letter)
+
+        return self.alphabet[idx]
+
     def randomize_alphabet(self):
         self.alphabet = list(string.ascii_uppercase)
         shuffle(self.alphabet)
 
+    def set_alphabet(self, alphabet):
+        if (len(alphabet) == 26):
+            self.alphabet = list(alphabet)
+
     def rotate(self, direction=1):
-        if direction == 1:
-            self.alphabet = self.alphabet[-1:] + self.alphabet[:-1]
-        elif direction == -1:
-            self.alphabet = self.alphabet
+        # if direction == 1:
+        #     self.alphabet = self.alphabet[-1:] + self.alphabet[:-1]
+        # elif direction == -1:
+        #     self.alphabet = self.alphabet
+
+        self.shift += direction
