8a162674fe6442be2b7b3953ef8a4da1355a0ddd
JakKacz <jakkacz@yahoo.com>
2018-04-13 21:07:56 +0200

    Add basie reflector methods

--- PS2/enigma/crypto/reflector.py
+++ PS2/enigma/crypto/reflector.py
@@ -1,3 +1,23 @@
+import string
+from random import shuffle
+
 class Reflector:
+
     def __init__(self):
-        pass
+        self.alphabet = self.init_alphabet()
+
+    def init_alphabet(self):
+        mixed_letters = list(string.ascii_uppercase)
+        shuffle(mixed_letters)
+
+        alphabet = {}
+        for char, index in string.ascii_uppercase:
+            alphabet[char] = mixed_letters[index]
+
+        return alphabet
+
+    def resolve_letter(self, letter):
+        return self.alphabet[letter]
+
+    def get_alphabet(self):
+        return self.alphabet
